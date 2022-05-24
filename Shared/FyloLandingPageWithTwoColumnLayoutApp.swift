751afe80019353e9e89088260d3d1c5a02a17a17
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-24 10:10:51 +0900

    Initial Commit

---
+++ Shared/FyloLandingPageWithTwoColumnLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  FyloLandingPageWithTwoColumnLayoutApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FyloLandingPageWithTwoColumnLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
